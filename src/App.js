c80608a093fbbba0695e97045a4a8e0dad381bdc
Bartosz008 <bbkaczy@gmail.com>
2022-02-21 19:42:34 +0100

    change fix travel

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import PropTypes from 'prop-types';
-import styles from './styles/AnimatedSwitch.module.scss';
+
 import MainLayout from './components/layout/MainLayout/MainLayout';
+
 import Home from './components/views/Home/Home';
 import Trips from './components/views/Trips/TripsContainer';
-// TODO - import other views
-import CountryContainer from'./components/views/Country/CountryContainer';
-import CountriesContainer from'./components/views/Countries/CountriesContainer';
-// import Trip from './components/views/Trip/Trip';
-import RegionsContainer from'./components/views/Regions/RegionsContainer';
+import Trip from './components/'; // TODO - import other views
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
-import { AnimatedSwitch } from 'react-router-transition';
+
 import parseTrips from './utils/parseTrips';
-import {setMultipleStates} from '../redux/globalRedux';
-import TripContainer from './components/views/Trip/TripContainer';
+import {setMultipleStates} from './redux/globalRedux';
 
 class App extends React.Component {
   static propTypes = {
@@ -41,21 +37,13 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <MainLayout>
-          <AnimatedSwitch
-            atEnter={{ opacity: 1, top:200}}
-            atLeave={{ opacity: 0, top: 0 }}
-            atActive={{ opacity: 1, top: 0 }}
-            className={styles.switchWrapper}>
+          <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/trips' component={Trips} />
             {/* TODO - add more routes for other views */}
-            <Route exact path='/countries' component={CountriesContainer} />
-            <Route exact path='/regions' component={RegionsContainer} />
-            <Route exact path='/trip/:id' component={TripContainer} />
-            <Route exact path='/country/:id' component={CountryContainer} />
             <Route exact path='/info' component={Info} />
             <Route path='*' component={NotFound} />
-          </AnimatedSwitch>
+          </Switch>
         </MainLayout>
       </BrowserRouter>
     );
